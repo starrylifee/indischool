382b6194fc112ff6c9953bc1abe9c34326947fc6
starrylife <forinnocen@naver.com>
2024-07-21 08:54:55 +0900

    githubhidden

--- pages/(실과) 5학년 계절에 맞는 옷 추천.py
+++ pages/(실과) 5학년 계절에 맞는 옷 추천.py
@@ -3,6 +3,19 @@ import pandas as pd
 import requests
 from openai import OpenAI
 
+hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK{ display: none; }
+    #MainMenu{ visibility: hidden; }
+    footer { visibility: hidden; }
+    header { visibility: hidden; }
+    </style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 # OpenWeatherMap API 키 설정
 API_KEY = st.secrets.get("openweathermap_api_key")
 OPENAI_API_KEY = st.sidebar.text_input("OpenAI API 키를 입력하세요.", type="password")
